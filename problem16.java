cf677d772c3c9b510f2be242bd4be8ddbfc43e17
monalidudhe28 <dudhemonika60@gmail.com>
2026-03-25 19:54:55 +0530

    Create problem16.java

---
+++ problem16.java
@@ -0,0 +1,48 @@
+16. A hospital manages different types of staff.
+Question:
+Create a base class Staff with method work().
+Create subclasses:
+ Doctor
+ Nurse
+ Receptionist
+Override work() to display different responsibilities.
+
+class Staff {
+    void work() {
+        System.out.println("Staff working");
+    }
+}
+
+class Doctor extends Staff {
+    void work() {
+        System.out.println("Doctor treats patients");
+    }
+}
+
+class Nurse extends Staff {
+    void work() {
+        System.out.println("Nurse cares for patients");
+    }
+}
+
+class Receptionist extends Staff {
+    void work() {
+        System.out.println("Receptionist manages appointments");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Staff s;
+
+        s = new Doctor();
+        s.work();
+
+        s = new Nurse();
+        s.work();
+
+        s = new Receptionist();
+        s.work();
+    }
+}
+
