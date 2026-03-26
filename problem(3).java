0008e15e5495c2bbc7941c42197fe0c0bc49d55c
monalidudhe28 <dudhemonika60@gmail.com>
2026-03-26 21:13:13 +0530

    Create problem(3).java

---
+++ problem(3).java
@@ -0,0 +1,26 @@
+// 3. Define a class StudentNameComparison.
+//  Accept two student names.
+//  Compare them using case-sensitive comparison.
+//  Compare them using case-insensitive comparison.
+//  Display appropriate message.
+
+import java.util.Scanner;
+class StudentNameComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name1 = sc.nextLine();
+        String name2 = sc.nextLine();
+
+        if (name1.equals(name2)) {
+            System.out.println("Names are equal (case-sensitive)");
+        } else {
+            System.out.println("Names are not equal (case-sensitive)");
+        }
+
+        if (name1.equalsIgnoreCase(name2)) {
+            System.out.println("Names are equal (case-insensitive)");
+        } else {
+            System.out.println("Names are not equal (case-insensitive)");
+        }
+    }
+}
