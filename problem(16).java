0eb192766c302fdabd5179ffdb8ff7576a3128b7
monalidudhe28 <dudhemonika60@gmail.com>
2026-03-29 21:47:50 +0530

    Create problem(16).java

---
+++ problem(16).java
@@ -0,0 +1,26 @@
+// 16. Music Player Application
+// Create a class MusicPlayer. Create thread using Thread class.
+// Display song progress using sleep.
+// Output: Playing song...
+// Progress 1
+// Progress 2
+// Progress 3.
+
+class MusicPlayer extends Thread {
+    public void run() {
+        System.out.println("Playing song...");
+        try {
+            for (int i = 1; i <= 3; i++) {
+                Thread.sleep(1000);
+                System.out.println("Progress " + i);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicPlayer player = new MusicPlayer();
+        player.start();
+    }
+}
